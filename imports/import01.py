d646e585f9a39962a36f2764f31ddf9121c4dcfa
Dhruti Kosta <koshtidhruti@noreply.com>
2020-07-06 20:42:15 -0400

    Week 1 & Week 2 - Day1

--- imports/import01.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    print("Welcome to class on Tuesday!")
-    print("Today we are learning about getting data into our scripts via APIs")
-
-if __name__ == "__main__":
-    main()
--- imports/import02.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import import01
-
-import01.main()
